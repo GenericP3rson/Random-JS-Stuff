9219eef6ab438f47c96088933f0bb6bc6512cd30
Paromita Datta <paromitadatta@Paromitas-MacBook-Pro.local>
2019-01-09 18:58:56 -0600

    Newest Addition to Card with Many Localhosts

--- Folder1.5/Folder1.53/index.js
+++ Folder1.5/Folder1.53/index.js
@@ -6,16 +6,20 @@ h.createServer((req, res) => {
     res.write(data);
     res.end();
   });
-}).listen(8080);
+}).listen(5000, 'localhost');
 
-// const h = require("http");
-// const f = require("fs");
+h.createServer((req, res) => {
+  fs.readFile('one.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}).listen(8080, 'localhost');
 
-// h.createServer((req, res)=>{
-//     f.readFile("one.html", (err, d)=>{
-//         if (err) throw err;
-//         res.writeHead(200, {'Content-Type': 'text/html'});
-//         res.write(d);
-//         res.end();
-//     });
-// }).listen(8080);
+h.createServer((req, res) => {
+  fs.readFile('one.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}).listen(3000, 'localhost');
